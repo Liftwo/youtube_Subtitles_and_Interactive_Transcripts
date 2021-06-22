5525b8b4362a506c63785907d7906d132453f7e6
zooeytsai <s1301h030@gmail.com>
2021-06-22 16:48:21 +0800

    heroku_1:prod&requirements.txt

--- yt_sub/prod_settings.py
+++ yt_sub/prod_settings.py
@@ -1,7 +1,6 @@
 from .settings import *
 import dj_database_url
 
-
 STATIC_URL = 'staticfiles'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
